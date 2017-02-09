cc51370b42cd6acee200115361a0ed206865876d
Creusel <victor.creusel@gmail.com>
2017-02-09 17:29:00 +0100

    Ecriture de verifier et toMIPS de affectationEntiere et VariableEntiere

--- src/plic/arbre/expression/Variable.java
+++ src/plic/arbre/expression/Variable.java
@@ -1,9 +1,9 @@
-package plic.arbre.variable;
+package plic.arbre.expression;
 
 import plic.arbre.ArbreAbstrait;
 import plic.tds.Entree;
 
-public abstract class Variable extends ArbreAbstrait {
+public abstract class Variable extends Expression {
 	protected Entree idf;
 	
 	public Variable(int no, Entree idf) {
--- src/plic/arbre/instruction/AffectationEntier.java
+++ src/plic/arbre/instruction/AffectationEntier.java
@@ -1,28 +1,32 @@
 package plic.arbre.instruction;
 
 import plic.arbre.expression.Expression;
-import plic.arbre.variable.VariableEntiere;
+import plic.arbre.expression.VariableEntiere;
 import plic.exceptions.AnalyseSemantiqueException;
+import plic.tds.Entree;
+import plic.tds.Symbole;
+import plic.tds.Tds;
 
 public class AffectationEntier extends Affectation {
 
-	public AffectationEntier(VariableEntiere idf, Expression droite) {
+	public AffectationEntier(Entree idf, Expression droite) {
 		super(idf, droite);
 	}
 
 	@Override
 	public void verifier() {
+		Tds tds = Tds.getInstance();
+		Symbole s = tds.identifier(idf);
 		if(droite.getType() != Expression.ENTIER){
 			throw new AnalyseSemantiqueException(getNoLigne(), 0, "L'expression droite n'est pas entière");
-		}
-		
-		//TODO : vérifier l'idf
-		
+		}		
 	}
 
 	@Override
 	public String toMIPS() {
-		return null; //TODO
+		Tds tds = Tds.getInstance();
+		Symbole s = tds.identifier(idf);
+		return droite.toMIPS() + "sw $v0, " + s.getDeplacement() + "($s7)\n";
 	}
 
 }
