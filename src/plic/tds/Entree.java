f09665d2b9e85b5afe2c5c9b44d553be2fdb54ba
Creusel <victor.creusel@gmail.com>
2017-02-21 18:02:13 +0100

    Ajout des variables dans la TDS à la déclaration. NON FONCTIONNEL

--- src/plic/tds/Entree.java
+++ src/plic/tds/Entree.java
@@ -3,9 +3,6 @@ package plic.tds;
 public class Entree {
 	private String nom;
 	
-	public static final int PUBLIQUE = 1, PRIVEE = 2;
-	
-	public static final int ENTIER = 1;
 	
 	public Entree(String n){
 		nom=n;
--- src/plic/tds/Symbole.java
+++ src/plic/tds/Symbole.java
@@ -1,10 +1,29 @@
 package plic.tds;
 
+import plic.exceptions.AnalyseSyntaxiqueException;
+
 public class Symbole {
 	private int spec;
 	private Type type;
 	private static int deplMem=0;
 	private int depl;
+	
+	
+	public static final int PUBLIQUE = 1, PRIVEE = 2;
+	
+	public static final int ENTIER = 1;
+	
+	//récupère le statut en format int d'une chaine
+	public static int statut(String s){
+		switch(s){
+			case "publique":
+				return PUBLIQUE;
+			case "privee":
+				return PRIVEE;
+			default:
+				throw new AnalyseSyntaxiqueException("Spécificateur d'accès attendu à la place de "+s);
+		}
+	}
 
 	public Symbole(int acces, Type t){
 		spec=acces;
